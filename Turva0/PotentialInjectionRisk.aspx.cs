ba5abd155e7a6467409f8367cfac232eba93e6c6
H8855 <H8855@MEDI-426-WS-04>
2016-11-17 10:57:07 +0200

    Turva-solution

---
+++ Turva0/PotentialInjectionRisk.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class PotentialInjectionRisk : System.Web.UI.Page
+{
+  protected void Page_Load(object sender, EventArgs e)
+  {
+        if (!IsPostBack)
+        {
+            txtDate.Text = DateTime.Now.ToShortDateString();
+        }
+  }
+
+    protected void btnAddToMySQL_Click(object sender, EventArgs e)
+    {
+        //kantaan ilman sanitointia
+        string nimi = txtName.Text;
+        string kommentti = txtComment.Text;
+        string pvm = txtDate.Text;
+        //string sql = string.Format("INSERT INTO comment (name, comment, date) VALUES ('{0}', '{1}', '{2}');", nimi, kommentti, pvm);
+        //parametreilla
+        string sql = string.Format("INSERT INTO comment (name, comment, date) VALUES (@name, @comment, @date);");
+        try
+        {
+            MySqlConnection conn = new MySqlConnection(myComments.ConnectionString);
+            conn.Open();
+            MySqlCommand cmd = new MySqlCommand(sql, conn);
+            //lisätään parametrit
+            cmd.Parameters.AddWithValue("@name", nimi);
+            cmd.Parameters.AddWithValue("@comment", kommentti);
+            cmd.Parameters.AddWithValue("@date", pvm);
+            int lkm = cmd.ExecuteNonQuery();
+            lblMessages.Text = string.Format("lisätty {0} kommenttia kantaan", lkm);
+            gvComments.DataBind();
+        }
+        catch (Exception ex)
+        {
+
+            lblMessages.Text = ex.Message;
+        }
+    }
+}
---
+++ Turva0/ValidateRequest.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class ValidateRequest : System.Web.UI.Page
+{
+    //http://stackoverflow.com/questions/502112/handling-request-validation-silently
+    //TODO ylikirjoita ProcessRequest
+    public override void ProcessRequest(HttpContext context)
+    {
+        try
+        {
+            base.ProcessRequest(context);
+        }
+        catch (HttpRequestValidationException ex)
+        {
+            context.Response.Redirect("HandleValidationError.aspx");
+        }
+    }
+    protected void Page_Load(object sender, EventArgs e)
+  {
+    try
+    {
+
+    }
+    catch (Exception ex)
+    {
+      ltMessage.Text = ex.Message;  
+    }
+  }
+
+  protected void btnCommit_Click(object sender, EventArgs e)
+  {
+    try
+    {
+            //TODO kirjoitetaan kommentti XML:ään
+            string kommenttifilu = Server.MapPath("~/App_Data/comments.xml");
+            DataSet ds = new DataSet();
+            ds.ReadXml(kommenttifilu);
+            string nimi = txtName.Text;
+            string kommentti = txtComment.Text;
+            string pvm = DateTime.Now.ToShortDateString();
+            string[] uusikommentti = { nimi, kommentti, pvm };
+            ds.Tables[0].Rows.Add(uusikommentti);
+            ds.WriteXml(kommenttifilu);
+            txtName.Text = string.Empty;
+            txtComment.Text = string.Empty;
+            gvComments.DataBind();
+    }
+    catch (Exception ex)
+    {
+      ltMessage.Text = ex.Message;
+    }
+  }
+}
